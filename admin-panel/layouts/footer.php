7b139d84501df7c7147ba26fd9f70bfc5788a139
Arunabh97 <dasarunabh036@gmail.com>
2024-03-29 19:26:15 +0530

    enhnaced the footer in admin panel

--- admin-panel/layouts/footer.php
+++ admin-panel/layouts/footer.php
@@ -1,11 +1,21 @@
-</div>
+<div id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                
+            </div>
+            <div class="col-md-4">
+                
+            </div>
+            <div class="col-md-4">
+                
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <hr>
+                <p>&copy; 2024 Coffee Blend. All rights reserved by Arunabh.</p>
+            </div>
+        </div>
     </div>
 </div>
-</div>
-</div>
-</div>
-<script type="text/javascript">
-
-</script>
-</body>
-</html>
